9e188b8dbd8326e20b2da8b7d5eebca1151022b8
dhane31 <dhanegm731@gmail.com>
2021-03-01 20:47:34 +0900

    Change UI my home page

---
+++ lib/my_home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> _todoItems = [
+    "英語の課題",
+    "牛乳を買う",
+  ];
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: _todoItems.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1.0, color: Colors.red)),
+              child: ListTile(
+                title: Text(_todoItems[index]),
+                trailing: IconButton(
+                  icon: Icon(Icons.more_vert),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
