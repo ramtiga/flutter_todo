526e4268d23d9780f47751940685bc491a521c49
dhane31 <dhanegm731@gmail.com>
2021-03-02 09:00:15 +0900

    Add todo class and update design

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconpicker/flutter_iconpicker.dart';
+import 'my_home_page.dart';
 
 class CreatePage extends StatefulWidget {
   @override
@@ -8,24 +10,54 @@ class CreatePage extends StatefulWidget {
 class _CreatePageState extends State<CreatePage> {
   String _title = "";
 
+  IconData _icon;
+
+  void _pickIcon() async {
+    IconData icon = await FlutterIconPicker.showIconPicker(context);
+    setState(() {
+      _icon = icon;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Create TODO"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text("TODOを入力してください"),
-            TextField(
-              onChanged: (String text) => _title = text,
-            ),
-            ElevatedButton(
-                onPressed: () => Navigator.pop(context, _title),
-                child: Text("Back"))
-          ],
+      body: Container(
+        padding: EdgeInsets.all(40.0),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: "TODO title"),
+                onChanged: (String text) => _title = text,
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 20.0, bottom: 30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _icon != null
+                        ? Icon(
+                            _icon,
+                            size: 45.0,
+                          )
+                        : Text("none"),
+                    ElevatedButton(
+                      child: const Text("Pick Icon"),
+                      onPressed: () => _pickIcon(),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () => Navigator.pop(context, Todo(_title, _icon)),
+                  child: Text("Back"))
+            ],
+          ),
         ),
       ),
     );
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -10,10 +10,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> _todoItems = [];
-  void _addTodo(String title) {
+  List<Todo> _todoItems = [
+    Todo("英語の課題", Icons.description),
+    Todo("牛乳を買う", Icons.local_grocery_store),
+  ];
+
+  void _addTodo(Todo todo) {
     setState(() {
-      _todoItems.add(title);
+      _todoItems.add(todo);
     });
   }
 
@@ -37,13 +41,17 @@ class _MyHomePageState extends State<MyHomePage> {
               decoration: BoxDecoration(
                   border: Border.all(width: 1.0, color: Colors.red)),
               child: ListTile(
-                title: Text(_todoItems[index]),
+                leading: Icon(
+                  _todoItems[index].icon,
+                  size: 35.0,
+                ),
+                title: Text(_todoItems[index].title),
                 trailing: IconButton(
                   icon: Icon(Icons.more_vert),
                   onPressed: () => showDialog(
                     context: context,
                     builder: (BuildContext context) => AlertDialog(
-                      title: Text(_todoItems[index]),
+                      title: Text(_todoItems[index].title),
                       actions: [
                         IconButton(
                           icon: Icon(Icons.delete),
@@ -64,13 +72,20 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          final String title = await Navigator.of(context)
+          final Todo todo = await Navigator.of(context)
               .push(MaterialPageRoute(builder: (context) => CreatePage()));
-          if (title != null && title != "") _addTodo(title);
+          if (todo != null) _addTodo(todo);
         },
         tooltip: 'Add Todo',
         child: Icon(Icons.add),
       ),
     );
   }
+}
+
+class Todo {
+  String title;
+  IconData icon;
+
+  Todo(this.title, this.icon);
 }
